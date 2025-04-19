4e0afa33023f0b4477c2ea6f8047b1143786e4ca
Shakil Malek <maleksameer528@gmail.com>
2025-04-19 16:29:52 +0530

    updated ssr

--- app/components/ui/Globe.tsx
+++ app/components/ui/Globe.tsx
@@ -342,11 +342,13 @@ export function WebGLRendererConfig() {
   const { gl, size } = useThree();
 
   useEffect(() => {
-    gl.setPixelRatio(window.devicePixelRatio);
-    gl.setSize(size.width, size.height);
-    gl.setClearColor(0xffaaff, 0);
+    if (typeof window !== "undefined") {
+      // The code will only run in the client-side (browser)
+      gl.setPixelRatio(window.devicePixelRatio);
+      gl.setSize(size.width, size.height);
+      gl.setClearColor(0xffaaff, 0);
+    }
   }, []);
-
   return null;
 }
 
